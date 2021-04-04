704f0a414f1b66ada74f62332c1427b477197890
will <will.eizlini@gmail.com>
2021-04-04 17:32:33 -0400

    user model

---
+++ src/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
